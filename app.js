eb24cff43dadaabd54fd73828b429d0a80ac981c
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 02:31:40 +0300

    Основной учебник. Пустой результат функции TypeScript. 15 раздел.

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-function sum(x, y) {
-    return x + y;
+function func(text) {
+    console.log(text);
 }
-console.log(sum(1, 2));
-var arr = [1, 2, 3];
-function sumArray(arr) {
-    var res = 0;
-    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-        var num = arr_1[_i];
-        res += num;
-    }
-    return res;
-}
-console.log(sumArray(arr));
+func('str');
--- app.ts
+++ app.ts
@@ -1,19 +1,4 @@
-function sum(x: number, y: number): number {
-    return x + y;
+function func(text: string): void {
+	console.log(text);
 }
-
-console.log(sum(1, 2));
-
-let arr: number[] = [1, 2, 3];
-
-function sumArray(arr: number[]): number {
-    let res = 0;
-
-    for (let num of arr) {
-        res += num;
-    }
-
-    return res;
-}
-
-console.log(sumArray(arr));
+func('test');
