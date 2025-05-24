a962548ca563b1c1066e0817e5b28838107844d2
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 02:49:44 +0300

    Основной учебник. Псевдонимы типов TypeScript. 19 раздел.

--- app.ts
+++ app.ts
@@ -1,8 +1,2 @@
-let varTest1: number | null;
-varTest1 = 1;
-varTest1 = null;
-
-let varTest2: number | string | boolean;
-varTest2 = 1;
-varTest2 = '1';
-varTest2 = false;
+type nulldefined = null | undefined;
+type bulldefined = boolean | null | undefined;
