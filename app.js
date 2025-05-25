405f2be22015a27cc2efab353d6c8e63cbdb2a54
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 05:05:27 +0300

    Основной учебник. Массивы в объектах в TypeScript. 43 раздел.

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-var obj = {
-    name: 'john',
-    age: 30,
-    salaryday: 1000,
+var country;
+country = {
+    name: 'Республика Беларусь',
+    cities: ['Минск', 'Брест', 'Гродно', 'Гомель', 'Витебск', 'Могилев']
 };
-var objTime1 = {
-    hour: 10,
-    minute: 10,
-    second: 10,
-};
-var objTime2 = {
-    hour: 10,
-    minute: 10,
-    second: 10,
-};
-var objTime3 = {
-    hour: 10,
-    minute: 10,
-    second: 10,
-};
-console.log(obj, objTime1, objTime2, objTime3);
+console.log(country);
--- app.ts
+++ app.ts
@@ -1,41 +1,11 @@
-interface Employee {
+let country: {
     name: string,
-    age: number,
-    salaryday: number
-}
+    cities: string[],
+};
 
-let obj: Employee = {
-    name: 'john',
-    age: 30,
-    salaryday: 1000,
-}
+country = {
+    name: 'Республика Беларусь',
+    cities: ['Минск', 'Брест', 'Гродно', 'Гомель', 'Витебск', 'Могилев'];
+};
 
-interface Time {
-    hour: number,
-    minute: number,
-    second: number
-}
-
-let objTime1: Time = {
-    hour: 10,
-    minute: 10,
-    second: 10,
-}
-
-let objTime2: Time = {
-    hour: 10,
-    minute: 10,
-    second: 10,
-}
-
-let objTime3: Time = {
-    hour: 10,
-    minute: 10,
-    second: 10,
-}
-
-console.log(
-    obj,
-    objTime1,
-    objTime2, objTime3
-)
+console.log(country);
