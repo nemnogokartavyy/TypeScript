6255bc6bb6a4a4f7910d14730d5f6dd049bcc6cc
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 08:38:26 +0300

    Основной учебник. Значения параметров по умолчанию в TypeScript. 50 раздел.

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-var daysWeek = [
-    'Воскресенье',
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота'
-];
-function currentDay(year, month, date) {
-    var now = new Date();
-    var y = year !== undefined ? year : now.getFullYear();
-    var m = month !== undefined ? month : now.getMonth();
-    var d = date !== undefined ? date : now.getDate();
-    var desDate = new Date(y, m, d);
-    var dayIndex = desDate.getDay();
-    return daysWeek[dayIndex];
+function degNum(number, degree) {
+    if (degree === void 0) { degree = 2; }
+    return Math.pow(number, degree);
 }
-// Примеры использования:
-console.log(currentDay()); // Сегодняшний день
-console.log(currentDay(2023, 10, 5)); // 5 ноября 2023
+console.log(degNum(4, 4));
--- app.ts
+++ app.ts
@@ -1,26 +1,5 @@
-let daysWeek: string[] = [
-    'Воскресенье',
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота'
-];
+function degNum(number: number, degree: number = 2): number {
+    return Math.pow(number, degree);
+}
 
-function currentDay(year?: number, month?: number, date?: number): string {
-    const now = new Date();
-
-    const y = year !== undefined ? year : now.getFullYear();
-    const m = month !== undefined ? month : now.getMonth();
-    const d = date !== undefined ? date : now.getDate();
-
-    const desDate = new Date(y, m, d);
-
-    const dayIndex = desDate.getDay();
-    
-    return daysWeek[dayIndex];
-};
-
-console.log(currentDay());
-console.log(currentDay(2023, 10, 5));
+console.log(degNum(4, 4));
