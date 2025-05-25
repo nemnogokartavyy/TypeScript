86a125147915a39dd98431fe88f75d9d1eceb0e6
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 03:32:15 +0300

    Основной учебник. Деструктуризация кортежа в TypeScript. 26 раздел.

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-var date;
-date = [1];
-console.log(date);
+var time = [12, 59, 59];
+var hours = time[0], minutes = time[1], seconds = time[2];
+console.log("".concat(hours, ":").concat(minutes, ":").concat(seconds));
--- app.ts
+++ app.ts
@@ -1,3 +1,3 @@
-let date: [number, number?, number?];
-date = [1];
-console.log(date);
+let time: [number, number, number] = [12, 59, 59];
+let [hours, minutes, seconds] = time;
+console.log(`${hours}:${minutes}:${seconds}`);
