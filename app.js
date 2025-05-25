013d464ee9db519eaff47e41e9660d9a0f06c2d0
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 03:32:15 +0300

    Основной учебник. Заполнение кортежа в TypeScript. 27 раздел.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-var date;
-date = [1];
-console.log(date);
+var tpl1;
+tpl1 = ['1', '2', 3, 4, 5, 6, 7];
+var tpl2;
+tpl2 = [1, true, '2', 'false'];
+console.log(tpl1, tpl2);
--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
-let date: [number, number?, number?];
-date = [1];
-console.log(date);
+let tpl1: [string, string, ...number[]];
+tpl1 = ['1', '2', 3, 4, 5, 6, 7];
+
+let tpl2: [number, boolean, ...string[]];
+tpl2 = [1, true, '2', 'false'];
+
+console.log(tpl1, tpl2);
