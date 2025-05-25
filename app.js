431ff70d7eafaed78d224c447657675bb5e6d4f1
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 04:05:47 +0300

    Основной учебник. Явные ключи перечислений в TypeScript. 32 раздел.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var daysOfWeek;
-(function (daysOfWeek) {
-    daysOfWeek[daysOfWeek["Monday"] = 0] = "Monday";
-    daysOfWeek[daysOfWeek["Tuesday"] = 1] = "Tuesday";
-    daysOfWeek[daysOfWeek["Wednesday"] = 2] = "Wednesday";
-    daysOfWeek[daysOfWeek["Thursday"] = 3] = "Thursday";
-    daysOfWeek[daysOfWeek["Friday"] = 4] = "Friday";
-    daysOfWeek[daysOfWeek["Saturday"] = 5] = "Saturday";
-    daysOfWeek[daysOfWeek["Sunday"] = 6] = "Sunday";
-})(daysOfWeek || (daysOfWeek = {}));
+var Months;
+(function (Months) {
+    Months[Months["January"] = 1] = "January";
+    Months[Months["February"] = 2] = "February";
+    Months[Months["March"] = 3] = "March";
+    Months[Months["April"] = 4] = "April";
+    Months[Months["May"] = 5] = "May";
+    Months[Months["June"] = 6] = "June";
+    Months[Months["July"] = 7] = "July";
+    Months[Months["August"] = 8] = "August";
+    Months[Months["September"] = 9] = "September";
+    Months[Months["October"] = 10] = "October";
+    Months[Months["November"] = 11] = "November";
+    Months[Months["December"] = 12] = "December";
+})(Months || (Months = {}));
 ;
+var one = Months.January;
+var five = Months.May;
+console.log(one, five);
--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-enum daysOfWeek { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+enum Months {
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12
+};
+
+let one: Months = Months.January;
+let five: Months = Months.May;
+console.log(one, five);
