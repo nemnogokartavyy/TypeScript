288c6083e99244cbc0b1fa365612db933f58b143
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 04:30:41 +0300

    Основной учебник. Тип HTMLElement в TypeScript. 38 раздел.

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-var reg = new RegExp('.+?');
-console.log(reg);
+var elementPromise;
+elementPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        var element = document.querySelector('div');
+        if (element) {
+            resolve(element);
+        }
+        else {
+            reject(new Error('Элемент не найден'));
+        }
+    }, 1000);
+});
+elementPromise.then(function (result) {
+    var element = result;
+    console.log('Полученный элемент:', element);
+}).catch(function (error) {
+    console.error(error);
+});
+var elem = document.createElement('div');
+document.body.append(elem);
+console.log(elem);
+var list = document.createElement('ul');
+document.body.append(list);
+console.log(list);
--- app.ts
+++ app.ts
@@ -1,2 +1,27 @@
-let reg: RegExp = new RegExp('.+?');
-console.log(reg);
+let elementPromise: Promise<any>;
+
+elementPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const element = document.querySelector('div');
+        if (element) {
+            resolve(element);
+        } else {
+            reject(new Error('Элемент не найден'));
+        }
+    }, 1000);
+});
+
+elementPromise.then((result) => {
+    const element = result as HTMLElement;
+    console.log('Полученный элемент:', element);
+}).catch((error) => {
+    console.error(error);
+});
+
+let elem: HTMLElement = document.createElement('div');
+document.body.append(elem);
+console.log(elem);
+
+let list: HTMLUListElement = document.createElement('ul');
+document.body.append(list);
+console.log(list);
