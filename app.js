2d59d34d8c351780225798620039a7b407eef467
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 03:17:37 +0300

    Основной учебник. Необязательные элементы кортежей в TypeScript. 25 раздел.

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-var date1;
-var date2;
-var date3;
-date1 = [2025, 5];
-date2 = [2025, 'may'];
-date3 = [2025, 5, 25];
-console.log(date1, date2, date3);
+var date;
+date = [1];
+console.log(date);
--- app.ts
+++ app.ts
@@ -1,9 +1,3 @@
-let date1: [number, number];
-let date2: [number, string];
-let date3: [number, number, number];
-
-date1 = [2025, 5];
-date2 = [2025, 'may'];
-date3 = [2025, 5, 25];
-
-console.log(date1, date2, date3);
+let date: [number, number?, number?];
+date = [1];
+console.log(date);
