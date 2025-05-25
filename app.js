1b39fd25aa2ed17404ce3b822b0789ea67c4ea71
nemnogokartavyy <iikorzun@yandex.by>
2025-05-25 03:15:35 +0300

    Основной учебник. Кортежи в TypeScript. 22 раздел.

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var str = 'error';
-console.log(str);
+var date1;
+var date2;
+var date3;
+date1 = [2025, 5];
+date2 = [2025, 'may'];
+date3 = [2025, 5, 25];
+console.log(date1, date2, date3);
--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-type test = 'error' | 'warning' | 'success';
-let str: test = 'error';
-console.log(str);
+let date1: [number, number];
+let date2: [number, string];
+let date3: [number, number, number];
+
+date1 = [2025, 5];
+date2 = [2025, 'may'];
+date3 = [2025, 5, 25];
+
+console.log(date1, date2, date3);
