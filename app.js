53b83a445283b7e6da69130658a7677c37165b30
nemnogokartavyy <iikorzun@yandex.by>
2025-05-28 19:29:53 +0300

    Высший учебник. Методы классов в ООП в TypeScript. 5 раздел.

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ var Student = /** @class */ (function () {
         this.name = 'Jack';
         this.age = 21;
     }
+    Student.prototype.getName = function () {
+        return this.name;
+    };
+    Student.prototype.setName = function (name) {
+        this.name = name;
+    };
+    Student.prototype.getAge = function () {
+        return this.age;
+    };
+    Student.prototype.setAge = function (age) {
+        this.age = age;
+    };
     return Student;
 }());
 var student = new Student;
+console.log(student.getName(), student.getAge());
+student.setName('Max');
+student.setAge(22);
 console.log(student.name, student.age);
--- app.ts
+++ app.ts
@@ -1,7 +1,26 @@
 class Student {
     name: string = 'Jack';
     age: number = 21;
+
+    getName(): string {
+        return this.name;
+    }
+
+    setName(name: string): void {
+        this.name = name;
+    }
+
+    getAge(): number {
+        return this.age;
+    }
+
+    setAge(age: number): void {
+        this.age = age;
+    }
 }
 
 let student: Student = new Student;
+console.log(student.getName(), student.getAge());
+student.setName('Max');
+student.setAge(22);
 console.log(student.name, student.age);
