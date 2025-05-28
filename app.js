9645eb9b4afc77b08e4f213cf99b53400fa1885f
nemnogokartavyy <iikorzun@yandex.by>
2025-05-28 18:15:41 +0300

    Высший учебник. Классы ООП в TypeScript. 2 раздел.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-var func1 = function (num1, num2) { return num1 + num2; };
-console.log(func1(1, 3));
-var func2 = function (str) { return str.split(''); };
-console.log(func2('qwerty'));
-var arr = [1, 2, 3];
-var res = arr.map(function (num) { return Math.pow(num, 2); });
-console.log(res);
+var Employee = /** @class */ (function () {
+    function Employee() {
+    }
+    return Employee;
+}());
--- app.ts
+++ app.ts
@@ -1,12 +1,3 @@
-let func1 = (num1: number, num2: number): number => num1 + num2;
+class Employee {
 
-console.log(func1(1, 3));
-
-let func2 = (str: string): string[] => str.split('');
-
-console.log(func2('qwerty'));
-
-let arr: number[] = [1, 2, 3];
-let res: number[] = arr.map((num: number) => num ** 2);
-
-console.log(res);
+}
