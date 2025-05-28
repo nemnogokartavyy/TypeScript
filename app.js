f1ce81e865ffe404d79b0aea34766af5b052aedf
nemnogokartavyy <iikorzun@yandex.by>
2025-05-28 22:13:00 +0300

    Высший учебник. Наследование ООП классов в TypeScript. 7 раздел.

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-var Employee = /** @class */ (function () {
-    function Employee(name, surname, age, salary) {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var User = /** @class */ (function () {
+    function User(name, surname, age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
-        this.salary = salary;
+    }
+    return User;
+}());
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, surname, age, salary) {
+        var _this = _super.call(this, name, surname, age) || this;
+        _this.salary = salary;
+        return _this;
     }
     return Employee;
-}());
-var employee1 = new Employee('Петя', 'Иванов', 24, 1000);
-var employee2 = new Employee('Константин', 'Ефимов', 34, 1200);
-console.log(employee1, employee2);
+}(User));
+var employee = new Employee('Иван', 'Иванов', 20, 1000);
+console.log(employee);
--- app.ts
+++ app.ts
@@ -1,18 +1,32 @@
-class Employee {
+class User {
     name: string;
     surname: string;
     age: number;
-    salary: number;
 
-    constructor(name: string, surname: string, age: number, salary: number) {
+    constructor(
+        name: string,
+        surname: string,
+        age: number) {
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+}
+
+class Employee extends User {
+    salary: number;
+
+    constructor(
+        name: string,
+        surname: string,
+        age: number,
+        salary: number
+    ) {
+        super(name, surname, age);
         this.salary = salary;
     }
 }
 
-let employee1: Employee = new Employee('Петя', 'Иванов', 24, 1000);
-let employee2: Employee = new Employee('Константин', 'Ефимов', 34, 1200);
+let employee: Employee = new Employee('Иван', 'Иванов', 20, 1000);
 
-console.log(employee1, employee2);
+console.log(employee);
