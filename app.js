3abfd7a4bf8a1fb5a2934699eaac40d97e4276e0
nemnogokartavyy <iikorzun@yandex.by>
2025-05-29 14:22:27 +0300

    Высший учебник. Модификатор readonly в TypeScript. 12 раздел.

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var User = /** @class */ (function () {
-    function User(name, surn) {
-        this.name = name;
-        this.surn = surn;
+    function User() {
+        this.age = 12;
     }
     return User;
 }());
-var Employee = /** @class */ (function (_super) {
-    __extends(Employee, _super);
-    function Employee(name, surn, salary) {
-        var _this = _super.call(this, name, surn) || this;
-        _this.salary = salary;
-        return _this;
-    }
-    Employee.prototype.getName = function () {
-        return this.name;
-    };
-    Employee.prototype.getSurn = function () {
-        return this.surn;
-    };
-    Employee.prototype.getSalary = function () {
-        return this.salary;
-    };
-    return Employee;
-}(User));
-var employee = new Employee("John", "Doe", 50000);
-console.log(employee.getName(), employee.getSurn(), employee.getSalary());
-employee.name = 'e';
+var user = new User;
+console.log(user.age);
+user.age = 11;
--- app.ts
+++ app.ts
@@ -1,33 +1,6 @@
 class User {
-    protected name: string;
-    protected surn: string;
-
-    constructor(name: string, surn: string) {
-        this.name = name;
-        this.surn = surn;
-    }
+    readonly age: number = 12;
 }
 
-class Employee extends User {
-    protected salary: number;
-    constructor(name: string, surn: string, salary: number) {
-        super(name, surn);
-        this.salary = salary;
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    getSurn() {
-        return this.surn;
-    }
-
-    getSalary() {
-        return this.salary;
-    }
-}
-
-let employee: Employee = new Employee("John", "Doe", 50000);
-
-console.log(employee.getName(), employee.getSurn(), employee.getSalary());
+let user: User = new User;
+console.log(user.age);
