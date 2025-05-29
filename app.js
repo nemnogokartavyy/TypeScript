e36fe458f5ae54a102e88aa727f99d7273a26f2f
nemnogokartavyy <iikorzun@yandex.by>
2025-05-29 14:59:38 +0300

    Высший учебник. Статические свойства в TypeScript. 14 раздел.

--- app.js
+++ app.js
@@ -1,53 +1,7 @@
-var User = /** @class */ (function () {
-    function User(name, age) {
-        this._name = name;
-        this._age = age;
+var Student = /** @class */ (function () {
+    function Student() {
     }
-    Object.defineProperty(User.prototype, "name", {
-        get: function () {
-            return this._name;
-        },
-        set: function (newName) {
-            this._name = newName;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(User.prototype, "age", {
-        get: function () {
-            return this._age;
-        },
-        set: function (newAge) {
-            if (newAge >= 0 && newAge <= 120) {
-                this._age = newAge;
-            }
-            else {
-                throw new Error("Возраст должен быть в диапазоне от 0 до 120");
-            }
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return User;
+    Student.specialty = 'слесарь';
+    return Student;
 }());
-var user = new User("Иван", 30);
-console.log(user.age);
-try {
-    user.age = -5;
-}
-catch (error) {
-    console.error(error.message);
-}
-try {
-    user.age = 150;
-}
-catch (error) {
-    console.error(error.message);
-}
-try {
-    user.age = 100;
-}
-catch (error) {
-    console.error(error.message);
-}
-console.log(user.age);
+console.log(Student.specialty);
--- app.ts
+++ app.ts
@@ -1,52 +1,5 @@
-class User {
-    private _name: string;
-    private _age: number;
-
-    constructor(name: string, age: number) {
-        this._name = name;
-        this._age = age;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    set name(newName: string) {
-        this._name = newName;
-    }
-
-    get age(): number {
-        return this._age;
-    }
-
-    set age(newAge: number) {
-        if (newAge >= 0 && newAge <= 120) {
-            this._age = newAge;
-        } else {
-            throw new Error("Возраст должен быть в диапазоне от 0 до 120");
-        }
-    }
+class Student {
+    public static specialty: string = 'слесарь';
 }
 
-const user = new User("Иван", 30);
-console.log(user.age);
-
-try {
-    user.age = -5;
-} catch (error) {
-    console.error(error.message);
-}
-
-try {
-    user.age = 150;
-} catch (error) {
-    console.error(error.message);
-}
-
-try {
-    user.age = 100;
-} catch (error) {
-    console.error(error.message);
-}
-
-console.log(user.age);
+console.log(Student.specialty);
