1e2e12c3541ca2fa3601500171d9a746f3c5cde1
nemnogokartavyy <iikorzun@yandex.by>
2025-05-29 13:12:47 +0300

    Высший учебник. Модификатор private в TypeScript. 10 раздел.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var User = /** @class */ (function () {
-    function User() {
-        this.name = 'John';
-        this.age = 1995;
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
     }
+    User.prototype.getName = function () {
+        return this.name;
+    };
     User.prototype.getAge = function () {
-        var dateNow = new Date;
-        return dateNow.getFullYear() - this.age;
+        return this.age;
+    };
+    User.prototype.setName = function (name) {
+        this.name = name;
+    };
+    User.prototype.setAge = function (age) {
+        this.age = age;
     };
     return User;
 }());
-var user = new User;
-console.log(user.getAge());
+var user = new User('Max', 31);
+console.log(user);
+console.log(user.getName(), user.getAge());
+user.setName('Jack');
+user.setAge(40);
+console.log(user.getName(), user.getAge());
--- app.ts
+++ app.ts
@@ -1,10 +1,33 @@
 class User {
-    public name: string = 'John';
-    public age: number = 1995;
-    getAge() {
-        let dateNow: Date = new Date;
-        return dateNow.getFullYear() - this.age;
+    private name;
+    private age;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
     }
+
+    getName() {
+        return this.name;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    setName(name: string) {
+        this.name = name;
+    }
+
+    setAge(age: number) {
+        this.age = age;
+    }
+
 }
-let user = new User;
-console.log(user.getAge())
+
+let user = new User('Max', 31);
+console.log(user);
+console.log(user.getName(), user.getAge());
+user.setName('Jack');
+user.setAge(40);
+console.log(user.getName(), user.getAge());
