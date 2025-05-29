eec40f3e0755dfb6ae3dbfb74518dd42f6a5d1f1
nemnogokartavyy <iikorzun@yandex.by>
2025-05-29 12:44:40 +0300

    Высший учебник. Модификатор public в TypeScript. 9 раздел.

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var User = /** @class */ (function () {
-    function User(name, surname, age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
+    function User() {
+        this.name = 'John';
+        this.age = 1995;
     }
+    User.prototype.getAge = function () {
+        var dateNow = new Date;
+        return dateNow.getFullYear() - this.age;
+    };
     return User;
 }());
-var Employee = /** @class */ (function (_super) {
-    __extends(Employee, _super);
-    function Employee(name, surname, age, salary) {
-        var _this = _super.call(this, name, surname, age) || this;
-        _this.salary = salary;
-        return _this;
-    }
-    return Employee;
-}(User));
-var employee = new Employee('Иван', 'Иванов', 20, 1000);
-console.log(employee);
+var user = new User;
+console.log(user.getAge());
--- app.ts
+++ app.ts
@@ -1,32 +1,10 @@
 class User {
-    name: string;
-    surname: string;
-    age: number;
-
-    constructor(
-        name: string,
-        surname: string,
-        age: number) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
+    public name: string = 'John';
+    public age: number = 1995;
+    getAge() {
+        let dateNow: Date = new Date;
+        return dateNow.getFullYear() - this.age;
     }
 }
-
-class Employee extends User {
-    salary: number;
-
-    constructor(
-        name: string,
-        surname: string,
-        age: number,
-        salary: number
-    ) {
-        super(name, surname, age);
-        this.salary = salary;
-    }
-}
-
-let employee: Employee = new Employee('Иван', 'Иванов', 20, 1000);
-
-console.log(employee);
+let user = new User;
+console.log(user.getAge())
