6d8c6231f3358a2bca8897b0a293ccbc2cf0b287
nemnogokartavyy <iikorzun@yandex.by>
2025-05-29 20:19:54 +0300

    Высший учебник. Статические методы в TypeScript. 16 раздел.

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var Student = /** @class */ (function () {
-    function Student() {
+var Calc = /** @class */ (function () {
+    function Calc() {
     }
-    Student.university = 'БГУ';
-    return Student;
+    Calc.getSum2 = function (arr) {
+        var sum = 0;
+        for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+            var elem = arr_1[_i];
+            sum += Math.pow(elem, 2);
+        }
+        return sum;
+    };
+    Calc.getSum3 = function (arr) {
+        var sum = 0;
+        for (var _i = 0, arr_2 = arr; _i < arr_2.length; _i++) {
+            var elem = arr_2[_i];
+            sum += Math.pow(elem, 3);
+        }
+        return sum;
+    };
+    return Calc;
 }());
-console.log(Student.university);
+console.log(Calc.getSum2([1, 2, 3, 4, 5]));
+console.log(Calc.getSum3([1, 2, 3, 4, 5]));
--- app.ts
+++ app.ts
@@ -1,5 +1,24 @@
-class Student {
-    public static university: string = 'БГУ';
+class Calc {
+    public static getSum2(arr: number[]): number {
+        let sum: number = 0;
+
+        for (let elem of arr) {
+            sum += Math.pow(elem, 2);
+        }
+
+        return sum;
+    }
+
+     public static getSum3(arr: number[]): number {
+        let sum: number = 0;
+
+        for (let elem of arr) {
+            sum += Math.pow(elem, 3);
+        }
+
+        return sum;
+    }
 }
 
-console.log(Student.university);
+console.log(Calc.getSum2([1,2,3,4,5]));
+console.log(Calc.getSum3([1,2,3,4,5]));
