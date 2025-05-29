8bd0240b2aa804f9f64a0f82772785e1e030719d
nemnogokartavyy <iikorzun@yandex.by>
2025-05-29 14:31:33 +0300

    Высший учебник. Аксессоры в ООП в TypeScript. 13 раздел.

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 var User = /** @class */ (function () {
-    function User() {
-        this.age = 12;
+    function User(name, age) {
+        this._name = name;
+        this._age = age;
     }
+    Object.defineProperty(User.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (newName) {
+            this._name = newName;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(User.prototype, "age", {
+        get: function () {
+            return this._age;
+        },
+        set: function (newAge) {
+            if (newAge >= 0 && newAge <= 120) {
+                this._age = newAge;
+            }
+            else {
+                throw new Error("Возраст должен быть в диапазоне от 0 до 120");
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     return User;
 }());
-var user = new User;
+var user = new User("Иван", 30);
 console.log(user.age);
-user.age = 11;
+try {
+    user.age = -5;
+}
+catch (error) {
+    console.error(error.message);
+}
+try {
+    user.age = 150;
+}
+catch (error) {
+    console.error(error.message);
+}
+try {
+    user.age = 100;
+}
+catch (error) {
+    console.error(error.message);
+}
+console.log(user.age);
--- app.ts
+++ app.ts
@@ -1,6 +1,52 @@
 class User {
-    readonly age: number = 12;
+    private _name: string;
+    private _age: number;
+
+    constructor(name: string, age: number) {
+        this._name = name;
+        this._age = age;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(newName: string) {
+        this._name = newName;
+    }
+
+    get age(): number {
+        return this._age;
+    }
+
+    set age(newAge: number) {
+        if (newAge >= 0 && newAge <= 120) {
+            this._age = newAge;
+        } else {
+            throw new Error("Возраст должен быть в диапазоне от 0 до 120");
+        }
+    }
 }
 
-let user: User = new User;
+const user = new User("Иван", 30);
+console.log(user.age);
+
+try {
+    user.age = -5;
+} catch (error) {
+    console.error(error.message);
+}
+
+try {
+    user.age = 150;
+} catch (error) {
+    console.error(error.message);
+}
+
+try {
+    user.age = 100;
+} catch (error) {
+    console.error(error.message);
+}
+
 console.log(user.age);
